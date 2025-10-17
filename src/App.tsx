9d494e26663dbab41b6f3bc8507e73b80ce2274a
Elvin Musayev <msayev02@gmail.com>
2025-10-17 18:12:13 +0400

    Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <div className="text-5xl">
+        <h1>Setup</h1>
+      </div>
+    </main>
+  );
 }
 
 export default App;
