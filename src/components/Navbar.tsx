08b976ec2f35a15b6b6f13bcb9ca46ed1dd17007
Elvin Musayev <msayev02@gmail.com>
2025-10-23 23:57:46 +0400

    Feat: create a navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,16 +4,32 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <div>
+    <nav className=" bg-black">
+      <div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-2">
         <button>
-          <Logo />
+          <Logo className="w-10 h-10" />
         </button>
-        <div>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
+        <div className="flex gap-3 text-white [&>a:hover]:text-accent [&>a]:transition-colors [&>a]:duration-200">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "text-accent" : "text-white"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? "text-accent" : "text-white"
+            }
+          >
+            About
+          </NavLink>
         </div>
-        <button>Sign up</button>
+        <button className="px-6 py-2 bg-accent-dblue rounded leading-none text-white">
+          Sign up
+        </button>
       </div>
     </nav>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="grid place-content-center h-dvh">
-      <h1 className="text-5xl font-roboto">Home</h1>
+    <div className="grid place-content-center h-dvh bg-black">
+      <h1 className="text-5xl font-roboto text-white">Home</h1>
     </div>
   );
 }
