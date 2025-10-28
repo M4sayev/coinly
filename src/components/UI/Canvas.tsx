cc49d83a65f320f692a266f78659b7df0639702b
Elvin Musayev <msayev02@gmail.com>
2025-10-28 10:48:55 +0400

    Feat: add slight jitter animation to the coin

--- src/components/UI/Canvas.tsx
+++ src/components/UI/Canvas.tsx
@@ -6,6 +6,27 @@ import Euro from "../../assets/euro_outline.svg";
 
 interface CanvasProps extends React.CanvasHTMLAttributes<HTMLCanvasElement> {}
 
+interface Particle {
+  x: number;
+  y: number;
+  tx: number;
+  ty: number;
+}
+
+const IMAGE_MAP: { [key: string]: string } = {
+  bitcoin: Bitcoin,
+  azn: Azn,
+  dollar: Dollar,
+  euro: Euro,
+};
+
+const PARTICLE_SPEED = 0.005;
+const IMG_SCALE = 0.8;
+const LEFT_OFFSET_COEFFICIENT = 0.05;
+const IMAGE_DENSITY = 6; // lower the value, more particles
+const JITTER = 70;
+const JITTER_THRESHOLD = 0.5;
+
 const Canvas: React.FC<CanvasProps> = (props) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -22,7 +43,10 @@ const Canvas: React.FC<CanvasProps> = (props) => {
     ctx.scale(scale, scale);
 
     const img = new Image();
-    img.src = Bitcoin;
+    img.src = IMAGE_MAP["bitcoin"];
+
+    const particles: Particle[] = [];
+
     img.onload = () => {
       const offscreen = document.createElement("canvas");
 
@@ -33,23 +57,19 @@ const Canvas: React.FC<CanvasProps> = (props) => {
 
       const imageData = offCtx?.getImageData(0, 0, img.width, img.height);
 
-      const particles: { x: number; y: number; tx: number; ty: number }[] = [];
+      const leftOffset = rect.width * LEFT_OFFSET_COEFFICIENT;
+      const topOffset = rect.height / 2 - (img.height * IMG_SCALE) / 2;
 
-      const scaleFactor = 0.8;
-      const leftOffset = rect.width * 0.05;
-      const topOffset = rect.height / 2 - (img.height * scaleFactor) / 2;
-
-      const step = 6;
-      for (let y = 0; y < img.height; y += step) {
-        for (let x = 0; x < img.width; x += step) {
+      for (let y = 0; y < img.height; y += IMAGE_DENSITY) {
+        for (let x = 0; x < img.width; x += IMAGE_DENSITY) {
           const index = (y * img.width + x) * 4;
           const alpha = imageData?.data[index + 3];
           if (alpha && alpha > 128) {
             particles.push({
               x: Math.random() * rect.width,
               y: Math.random() * rect.height,
-              tx: x * scaleFactor + leftOffset,
-              ty: y * scaleFactor + topOffset,
+              tx: x * IMG_SCALE + leftOffset,
+              ty: y * IMG_SCALE + topOffset,
             });
           }
         }
@@ -64,17 +84,25 @@ const Canvas: React.FC<CanvasProps> = (props) => {
           img,
           leftOffset,
           topOffset,
-          img.width * scaleFactor,
-          img.height * scaleFactor
+          img.width * IMG_SCALE,
+          img.height * IMG_SCALE
         );
 
         ctx.globalCompositeOperation = "source-over";
 
-        ctx.fillStyle = "#668affff";
+        ctx.fillStyle = "#00124d";
 
         particles.forEach((p) => {
-          p.x += (p.tx - p.x) * 0.005;
-          p.y += (p.ty - p.y) * 0.005;
+          if (
+            Math.abs(p.x - p.tx) < JITTER_THRESHOLD &&
+            Math.abs(p.y - p.ty) < JITTER_THRESHOLD
+          ) {
+            p.x += Math.random() * JITTER - Math.random() * JITTER;
+            p.y += Math.random() * JITTER - Math.random() * JITTER;
+          }
+
+          p.x += (p.tx - p.x) * PARTICLE_SPEED;
+          p.y += (p.ty - p.y) * PARTICLE_SPEED;
 
           ctx.beginPath();
           ctx.arc(p.x, p.y, 1.5, 0, Math.PI * 2);
