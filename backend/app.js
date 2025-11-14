332a549458b8f8600706e6d3db0b8fbc4f688129
Elvin Musayev <msayev02@gmail.com>
2025-11-14 08:29:04 +0400

    Feat: impose a rate Limit on the market Api

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const marketRoutes = require("./routes/marketRoutes");
 
+const rateLimit = require("express-rate-limit");
+
+const marketlimit = rateLimit({
+  windowMs: 30 * 1000,
+  max: 50,
+  message: {
+    success: false,
+    message: "Too many requests, try again later...",
+  },
+});
+
 require("dotenv").config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1/market", marketRoutes);
+app.use("/api/v1/market", [marketlimit, marketRoutes]);
 
 const PORT = process.env.PORT || 5000;
 
